97f15d2de0656552841c009e4c019060a86fcdc6
cjustman2@my.wctc.edu <>
2013-01-31 16:52:40 -0600

    commit2

--- src/lab1/ProgrammingClasses.java
+++ src/lab1/ProgrammingClasses.java
@@ -4,10 +4,63 @@
  */
 package lab1;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author chris
  */
 public abstract class ProgrammingClasses {
+    private double credits;
+    private String courseName;
+    private String prerequisites;
+    private String courseNumber;
+    
+    
+        public double getCredits() {
+        return credits;
+    }
+
+    public void setCredits(double credits) {
+        if(credits < 0.5 || credits > 4.0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: credits must be in the range 0.5 to 4.0");
+            System.exit(0);
+        }
+        this.credits = credits;
+    }
+    
+       public String getCapitalizedCourseName() {
+        return this.getCourseName().toUpperCase();
+    }
+    
+        public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+    
+        public String getPrerequisites() {
+        return prerequisites;
+    }
+
+    public void setPrerequisites(String prerequisites) {
+        if(prerequisites == null || prerequisites.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: prerequisites cannot be null of empty string");
+            System.exit(0);
+        }
+        this.prerequisites = prerequisites;
+    }
+    
+      public String getCourseNumber() {
+        return courseNumber;
+    }
+
+    public void setCourseNumber(String courseNumber) {
+        this.courseNumber = courseNumber;
+    }
     
 }
