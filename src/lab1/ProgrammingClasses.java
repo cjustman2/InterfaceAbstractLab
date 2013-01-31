58cbee8679ee13351822ca3459d3285a676a8553
cjustman2@my.wctc.edu <>
2013-01-31 16:28:36 -0600

    comments done

---
+++ src/lab1/ProgrammingClasses.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+/**
+ *
+ * @author chris
+ */
+public abstract class ProgrammingClasses {
+    
+}
